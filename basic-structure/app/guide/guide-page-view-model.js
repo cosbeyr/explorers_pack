78c108e732886d2aacd4132cdf77a8be1aa77a64
kimbalm2 <kimbalm2@wwu.edu>
2019-11-02 16:20:38 -0700

    Made a grid layout, added an icon but not the intended one. Needs more still needs more styling.

--- basic-structure/app/guide/guide-page-view-model.js
+++ basic-structure/app/guide/guide-page-view-model.js
@@ -1,4 +1,5 @@
-const observableModule = require("tns-core-modules/data/observable");
+var observableModule = require("tns-core-modules/data/observable");
+var ObservableArray = require("tns-core-modules/data/observable-array").ObservableArray;
 function dataLoader() {
     const data = "Animals\n" +
         "Apps\n" +
@@ -37,16 +38,16 @@ function dataLoader() {
     dataArr.map((subject) => {
         const tempObj = {};
         tempObj.name = subject;
-        tempObj.description = `Description for ${subject}`;
-        console.log(tempObj);
+        tempObj.description = `Description for ${subject}`;//wut
+        //console.log(tempObj);
         template.items.push(tempObj);
     });
 
     return template;
 }
 function GuideViewModel() {
-    const viewModel = observableModule.fromObject(dataLoader());
-
+    var viewModel = new ObservableArray(dataLoader().items);
+    console.log(viewModel);
     return viewModel;
 }
 module.exports = GuideViewModel;
