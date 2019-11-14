b52f8538ad910c910623eebd8eecb5ed80c2d39e
mitch <kimbalm2@wwu.edu>
2019-11-13 20:26:33 -0600

    Added the floating action button.

--- app/src/main/java/com/WWU/explorerspack/MainActivity.java
+++ app/src/main/java/com/WWU/explorerspack/MainActivity.java
@@ -1,15 +1,21 @@
 package com.WWU.explorerspack;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.WWU.explorerspack.ui.guide.GuideListFragment;
 import com.WWU.explorerspack.ui.guide.ChapterData.ChapterContent;
 import com.WWU.explorerspack.ui.guide.L2.ChapterPageFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
@@ -21,12 +27,31 @@ public class MainActivity extends AppCompatActivity implements GuideListFragment
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        fab.hide();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+       navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+           @Override
+           public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+               if (destination.getId() == R.id.navigation_notifications)
+               fab.show();
+               else
+                   fab.hide();
+
+           }
+       });
        // this.navController = navController;
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
