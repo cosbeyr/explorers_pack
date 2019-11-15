5c14900f4bee1b006438f255feabbed45b35eb41
loc <loctruong96@gmail.com>
2019-11-14 22:27:54 -0800

    added JSON on disk for app settings and hikes info added ability save picture to gallery using settings added ability to save picture to a hike

--- app/src/main/java/com/WWU/explorerspack/MainActivity.java
+++ app/src/main/java/com/WWU/explorerspack/MainActivity.java
@@ -1,23 +1,114 @@
 package com.WWU.explorerspack;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.WWU.explorerspack.ui.guide.GuideListFragment;
 import com.WWU.explorerspack.ui.guide.ChapterData.ChapterContent;
 import com.WWU.explorerspack.ui.guide.L2.ChapterPageFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Dictionary;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements GuideListFragment.OnListFragmentInteractionListener{
     private NavController navController;
+
+
+    private boolean create(Context context, String fileName, String jsonString){
+        String FILENAME = "storage.json";
+        try {
+            FileOutputStream fos = context.openFileOutput(fileName,Context.MODE_PRIVATE);
+            if (jsonString != null) {
+                fos.write(jsonString.getBytes());
+            }
+            fos.close();
+            return true;
+        } catch (FileNotFoundException fileNotFound) {
+            return false;
+        } catch (IOException ioException) {
+            return false;
+        }
+
+    }
+
+    public boolean isFilePresent(Context context, String fileName) {
+        String path = context.getFilesDir().getAbsolutePath() + "/" + fileName;
+        File file = new File(path);
+        return file.exists();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        boolean isFilePresent = isFilePresent(this, "storage.json");
+        if(!isFilePresent) {
+            JSONObject template = new JSONObject();
+            JSONObject settings = new JSONObject();
+            JSONObject hikeLogs = new JSONObject();
+            JSONObject hike = new JSONObject();
+            JSONObject photo = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            try {
+                photo.put("JPEG_20191114_152929_8217285707265593563.jpg", "/storage/emulated/0/JPEG_20191114_152929_8217285707265593563.jpg");
+                jsonArray.put(photo);
+            }  catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+            try {
+                settings.put("saveToGallery", true);
+                settings.put("homePageTips", true);
+                settings.put("darkMode", true);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                hike.put("notes", "example notes...");
+                hike.put("map","map path on disk for processing");
+                hike.put("photos", jsonArray);
+                hikeLogs.put("example hike", hike);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                template.put("settings", settings);
+                template.put("hikeLogs", hikeLogs);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            boolean isFileCreated = create(this, "storage.json", template.toString());
+            if(isFileCreated) {
+                //proceed with storing the first todo  or show ui
+            } else {
+                Log.e("main","can't create storage.json");
+            }
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -30,6 +121,17 @@ public class MainActivity extends AppCompatActivity implements GuideListFragment
        // this.navController = navController;
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        List<Fragment> fragments = getSupportFragmentManager().getFragments();
+        if (fragments != null) {
+            for (Fragment fragment : fragments) {
+                fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
     }
 
     @Override
--- app/src/main/java/com/WWU/explorerspack/ui/camera/CameraFragment.java
+++ app/src/main/java/com/WWU/explorerspack/ui/camera/CameraFragment.java
@@ -1,79 +1,83 @@
 package com.WWU.explorerspack.ui.camera;
 
 import android.Manifest;
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.health.SystemHealthManager;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
+import androidx.fragment.app.DialogFragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.WWU.explorerspack.R;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
 
-import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
-import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 import static android.app.Activity.RESULT_OK;
-import static android.content.pm.PackageManager.PERMISSION_GRANTED;
-import static android.os.Environment.getExternalStoragePublicDirectory;
 
-public class CameraFragment extends Fragment implements View.OnClickListener {
+public class CameraFragment extends DialogFragment {
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
-    private static String[] PERMISSIONS_STORAGE = {
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-    };
     private CameraViewModel cameraViewModel;
     private ImageView imageView;
     private Button takePicture;
     private String currentPhotoPath;
-    static final int CAMERA_REQUEST_CODE = 1;
-    /**
-     * Checks if the app has permission to write to device storage
-     *
-     * If the app does not has permission then the user will be prompted to grant permissions
-     *
-     * @param activity
-     */
-    public static void verifyStoragePermissions(Activity activity) {
-        // Check if we have write permission
-        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    private static final int CAMERA_REQUEST_CODE = 1;
+    private JSONObject mainStorage;
+    private String[] hikeArray;
+    private Button yes, no;
+    private AlphaAnimation buttonClick = new AlphaAnimation(1F, 0.8F);
 
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            // We don't have permission so prompt the user
-            ActivityCompat.requestPermissions(
-                    activity,
-                    PERMISSIONS_STORAGE,
-                    REQUEST_EXTERNAL_STORAGE
-            );
-        }
+
+
+    public static void addImageToGallery(final String filePath, final Context context) {
+
+        ContentValues values = new ContentValues();
+
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, filePath);
+
+        context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
     }
+
     private void dispatchTakePictureIntent() {
         Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (cameraIntent.resolveActivity(getContext().getPackageManager())
@@ -87,7 +91,7 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
                    AndroidStudio complained.
 
                 */
-                System.out.println("Trying to take pic..");
+                Log.i("INFO","Trying to take pic..");
                 photoFile = createImageFile();
                 System.out.printf("PhotoFile Path %s\n",photoFile);
 
@@ -109,16 +113,33 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
                     Toast.LENGTH_SHORT).show();
         }
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_EXTERNAL_STORAGE) {
+            if (permissions[0].equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.i("info", "permission granted");
+            }
+        }
+    }
 
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        // getExternalFilesDir is private to app only, can't add to gallery yet... extra features
-        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        // require complex permission management to works....
-//        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        // getExternalFilesDir is private to app only
+        // File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        // File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStorageDirectory();
+
+        // must check permission at run time
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions( //Method of Fragment
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
+
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -129,7 +150,6 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
         currentPhotoPath = image.getAbsolutePath();
         return image;
     }
-
 
     private void setPic() {
         // Get the dimensions of the View
@@ -159,40 +179,93 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
     public void onActivityResult(int requestCode,
                                  int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //CALL THIS METHOD EVER
         if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-
-            // This will add the photo to the users gallery
-            galleryAddPic();
-
-            /* This adds the image to the screen.
-               For this app this creates an imageview each time a photo
-               is taken and adds it to a Flexbox layout
-           */
+            try{
+                // only add to Gallery of settings say so. Yes by default
+                JSONObject settings = mainStorage.getJSONObject("settings");
+                boolean saveToGallery =  settings.getBoolean("saveToGallery");
+                if (saveToGallery) {
+                    addImageToGallery(currentPhotoPath, getActivity());
+                }
+            } catch (JSONException e){
+                e.printStackTrace();
+            }
 
             setPic();
-
             Toast.makeText(getActivity(), "Photo Taken And Saved",
                     Toast.LENGTH_SHORT).show();
+
+            showAddToHike();
         } else {
             Toast.makeText(getActivity(), "The photo was not saved",
                     Toast.LENGTH_SHORT).show();
         }
     }
 
-    private void galleryAddPic() {
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File f = new File(currentPhotoPath);
-        Uri contentUri = Uri.fromFile(f);
-        mediaScanIntent.setData(contentUri);
-        getActivity().sendBroadcast(mediaScanIntent);
+
+    private void showAddToHike(){
+
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setCancelable(true);
+
+        View view  = getActivity().getLayoutInflater().inflate(R.layout.add_to_hike_dialog, null);
+        dialog.setContentView(view);
+
+        yes = (Button) dialog.findViewById(R.id.btn_yes);
+        no = (Button) dialog.findViewById(R.id.btn_no);
+
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                dialog.dismiss();
+                if(hikeArray.length > 0){
+                    showHikes();
+                } else {
+                    Toast.makeText(getActivity(), "You haven't started any hike yet!",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.startAnimation(buttonClick);
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
+    private void showHikes(){
+        AlertDialog.Builder window = new AlertDialog.Builder(getActivity());;
+        window.setTitle("Select a hike")
+                .setItems(hikeArray, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        try{
+                            // load data
+                            JSONObject hikeLogs = mainStorage.getJSONObject("hikeLogs");
+                            JSONObject selectedHike = hikeLogs.getJSONObject(hikeArray[item]);
+                            JSONArray photos = selectedHike.getJSONArray("photos");
+                            JSONObject photo = new JSONObject();
 
-    @Override
-    public void onClick(View view)
-    {
-        dispatchTakePictureIntent();
+                            // add to photos
+                            photo.put(currentPhotoPath.split("/")[currentPhotoPath.split("/").length-1], currentPhotoPath);
+                            photos.put(photo);
+                            create(getActivity(), "storage.json", mainStorage.toString());
+                            Toast.makeText(getActivity(), "Added photo to " + hikeArray[item] + " successfully.",
+                                    Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                            Toast.makeText(getActivity(), "Failed to add photo to " + hikeArray[item],
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        window.show();
     }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -200,16 +273,75 @@ public class CameraFragment extends Fragment implements View.OnClickListener {
         cameraViewModel =
                 ViewModelProviders.of(this).get(CameraViewModel.class);
         View root = inflater.inflate(R.layout.fragment_camera, container, false);
-        final TextView textView = root.findViewById(R.id.text_camera);
         imageView = root.findViewById(R.id.image_view);
         takePicture = root.findViewById(R.id.button);
-        takePicture.setOnClickListener(this);
-        cameraViewModel.getText().observe(this, new Observer<String>() {
+        String storage = read(getActivity(), "storage.json");
+
+        try {
+            mainStorage = new JSONObject(storage);
+            JSONObject hikes = mainStorage.getJSONObject("hikeLogs");
+            Iterator<String> keys = hikes.keys();
+            List<String> hikeList = new ArrayList<>();
+
+            while(keys.hasNext()) {
+                String key = keys.next();
+                hikeList.add(key);
+            }
+            hikeArray = new String[hikeList.size()];
+            for(int i = 0; i < hikeList.size(); i++){
+                hikeArray[i] = hikeList.get(i);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), "Failed to load settings",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+
+        takePicture.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+
             }
         });
         return root;
     }
+
+    private String read(Context context, String fileName) {
+        try {
+            FileInputStream fis = context.openFileInput(fileName);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+            }
+            fis.close();
+            return sb.toString();
+        } catch (FileNotFoundException fileNotFound) {
+            return null;
+        } catch (IOException ioException) {
+            return null;
+        }
+    }
+
+    private boolean create(Context context, String fileName, String jsonString){
+        String FILENAME = "storage.json";
+        try {
+            FileOutputStream fos = context.openFileOutput(fileName,Context.MODE_PRIVATE);
+            if (jsonString != null) {
+                fos.write(jsonString.getBytes());
+            }
+            fos.close();
+            return true;
+        } catch (FileNotFoundException fileNotFound) {
+            return false;
+        } catch (IOException ioException) {
+            return false;
+        }
+
+    }
+
 }
