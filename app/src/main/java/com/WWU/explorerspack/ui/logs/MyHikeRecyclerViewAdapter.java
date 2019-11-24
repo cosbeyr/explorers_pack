9a3cd99dfe1cc44dac728e052bf5c25023a7f4db
loc <loctruong96@gmail.com>
2019-11-23 16:13:49 -0800

    add code to remove hike and all the dialogs for it

--- app/src/main/java/com/WWU/explorerspack/ui/logs/MyHikeRecyclerViewAdapter.java
+++ app/src/main/java/com/WWU/explorerspack/ui/logs/MyHikeRecyclerViewAdapter.java
@@ -1,16 +1,28 @@
 package com.WWU.explorerspack.ui.logs;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
+import com.WWU.explorerspack.utilities.StorageUtilities;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.WWU.explorerspack.R;
 import com.WWU.explorerspack.ui.logs.HikeFragment.OnListFragmentInteractionListener;
 import com.WWU.explorerspack.ui.logs.hike_item.HikeList.HikeItem;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -32,6 +44,7 @@ public class MyHikeRecyclerViewAdapter extends RecyclerView.Adapter<MyHikeRecycl
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_hike, parent, false);
+
         return new ViewHolder(view);
     }
 
@@ -50,6 +63,10 @@ public class MyHikeRecyclerViewAdapter extends RecyclerView.Adapter<MyHikeRecycl
                 }
             }
         });
+
+    }
+    private void update(){
+        this.notifyDataSetChanged();
     }
 
     @Override
@@ -62,12 +79,48 @@ public class MyHikeRecyclerViewAdapter extends RecyclerView.Adapter<MyHikeRecycl
         public final TextView mIdView;
         public final TextView mContentView;
         public HikeItem mItem;
-
+        public Button removeButton;
         public ViewHolder(View view) {
             super(view);
             mView = view;
             mIdView = (TextView) view.findViewById(R.id.hike_id);
             mContentView = (TextView) view.findViewById(R.id.content);
+            removeButton = (Button) view.findViewById(R.id.remove_hike);
+            removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final String hike_to_be_removed = mValues.get(getAdapterPosition()).id;
+                    final String storage = StorageUtilities.read(v.getContext(), StorageUtilities.jsonStorageName);
+                    final Context inner = v.getContext();
+                    new AlertDialog.Builder(inner)
+                            .setTitle("Delete hike")
+                            .setMessage("Are you sure you want to delete this hike?")
+
+                            // Specifying a listener allows you to take an action before dismissing the dialog.
+                            // The dialog is automatically dismissed when a dialog button is clicked.
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // Continue with delete operation
+                                    Log.i("info", "deleted");
+                                    try {
+                                        mValues.remove(getAdapterPosition());
+                                        JSONObject hikes = new JSONObject(storage);
+                                        hikes.remove(hike_to_be_removed);
+                                        StorageUtilities.create(inner, StorageUtilities.jsonStorageName, hikes.toString());
+                                        update();
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            })
+
+                            // A null listener allows the button to dismiss the dialog and take no further action.
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            });
+
         }
 
         @Override
